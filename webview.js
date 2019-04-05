566a1c4cb69117fbc6fdf27306f31b1395e8fcd5
RIO Sasaki <sasaki@rio.ne.jp>
2019-04-05 16:59:44 +0900

    Chatworkの仕様変更

--- webview.js
+++ webview.js
@@ -6,12 +6,13 @@ module.exports = (Franz, options) => {
     let individualCount = 0;
     let directCount = 0;
     let indirectCount = 0;
-    let roomInfoContainer = document.querySelectorAll('li.roomListItem');
+    let roomInfoContainer = document.querySelectorAll('li.sc-dnqmqq');
 
     Array.prototype.forEach.call(roomInfoContainer, function (room) {
       let count = 0;
-      let unreadBadge = room.querySelector("span.roomListBadges__unreadBadgeText");
-      let unreadBadgeHasMention = room.querySelector("li.roomListBadges__unreadBadge--hasMemtion");
+
+      let unreadBadge = room.querySelector("span.sc-kAzzGY");
+      let unreadBadgeHasMention = room.querySelector("li._unreadBadge.sc-cSHVUG");
       if (unreadBadge && unreadBadge.innerText) {
         count = parseInt(unreadBadge.innerText);
       }
